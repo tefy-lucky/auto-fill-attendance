73a6659d3769d4d5c0144b536c5fc4458272734a
tefy <tefy.rakotomahefa@gmail.com>
2020-10-05 20:12:46 +0300

    :recycle: refactoring in progress

--- sheets-autofill.py
+++ sheets-autofill.py
@@ -1,5 +1,6 @@
 import pickle
 import os.path
+import os
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
@@ -8,7 +9,8 @@ import string
 import configparser
 
 config = configparser.ConfigParser()
-config.read('config.ini')
+cwd = os.path.dirname(os.path.abspath(__file__))
+config.read(os.path.join(cwd, "config.ini"))
 
 alphabet = list(string.ascii_uppercase)
 
@@ -26,20 +28,21 @@ def generate_columns(starting_column):
         if i == len(alphabet) - 1:
             for letter in alphabet:
                 for second_letter in alphabet:
-                    columns.append(f'{letter}{second_letter}')
+                    columns.append(f"{letter}{second_letter}")
 
     return columns
 
 
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 
+sections = config.sections()
 SAMPLE_SPREADSHEET_ID_input = config["default"]["spreadsheet_id"]
-SAMPLE_RANGE_NAME = 'July 2020!A12:A32'
+SAMPLE_RANGE_NAME = "Octobre 2020!A12:A32"
 
-row_gf = '12'
-row_me = '32'
+row_gf = "11"
+row_me = "30"
 
-column = 'A'  # beginning of column
+column = "A"  # beginning of column
 COLUMNS = generate_columns(starting_column=column)
 print(COLUMNS)
 
@@ -47,11 +50,10 @@ print(COLUMNS)
 # we need to check if we try to launch the script in the same day
 # in this case, there is no need to update anything
 new_day = False
-if config["default"]["date"] == datetime.today().strftime('%Y-%m-%d'):
+if config["default"]["date"] == datetime.today().strftime("%Y-%m-%d"):
     print("yoh! same day bro, same day...")
 else:
     new_day = True
-
 # we now have what we need: the starting column and the row number
 # now it's time to check the corresponding column for a given date
 # as I wrote those lines, we are 2020-07-19 and the corresponding column is "U" for this month
@@ -64,8 +66,8 @@ if new_day:
             column = COLUMNS[i + 1]
             break
 
-    config_file = open('config.ini', 'w+')
-    config.set("default", "date", datetime.today().strftime('%Y-%m-%d'))
+    config_file = open("config.ini", "w+")
+    config.set("default", "date", datetime.today().strftime("%Y-%m-%d"))
     config.set("default", "corresponding_column", column)
     config.write(config_file)
     config_file.close()
@@ -74,24 +76,25 @@ if new_day:
 def main():
     creds = None
 
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
+    if os.path.exists("token.pickle"):
+        with open("token.pickle", "rb") as token:
             creds = pickle.load(token)
 
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
             creds = flow.run_local_server(port=0)
-        with open('token.pickle', 'wb') as token:
+        with open("token.pickle", "wb") as token:
             pickle.dump(creds, token)
 
-    service = build('sheets', 'v4', credentials=creds)
+    service = build("sheets", "v4", credentials=creds)
 
     attendance = ""
     if datetime.today().weekday() == 5 or datetime.today().weekday() == 6:
         print("it's the weekend, what the hell are you doing?")
+        return
     else:
         attendance = "1"
     values = [
@@ -101,17 +104,21 @@ def main():
         ],
         # Additional rows ...
     ]
-    body = {
-        'values': values
-    }
+    body = {"values": values}
     sheet = service.spreadsheets()
-    sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID_input,
-                          range=f'Aout!{column}{row_gf}:{column}{row_gf}',
-                          valueInputOption='USER_ENTERED', body=body).execute()
-    sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID_input,
-                          range=f'Aout!{column}{row_me}:{column}{row_me}',
-                          valueInputOption='USER_ENTERED', body=body).execute()
+    sheet.values().update(
+        spreadsheetId=SAMPLE_SPREADSHEET_ID_input,
+        range=f"Octobre!{column}{row_gf}:{column}{row_gf}",
+        valueInputOption="USER_ENTERED",
+        body=body,
+    ).execute()
+    sheet.values().update(
+        spreadsheetId=SAMPLE_SPREADSHEET_ID_input,
+        range=f"Octobre!{column}{row_me}:{column}{row_me}",
+        valueInputOption="USER_ENTERED",
+        body=body,
+    ).execute()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
